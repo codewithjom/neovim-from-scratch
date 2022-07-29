1f507c77ceb93f29f9ec97e320a22a090421e6eb
Jom Dollesin <codewithjom@gmail.com>
2022-07-29 08:58:47 +0800

    Major updates, copied from dotfiles.

--- lua/user/coc.lua
+++ lua/user/coc.lua
@@ -1,12 +1,14 @@
 vim.cmd [[
-let g:coc_global_extension = [
-\ 'coc-snippets',
-\ 'coc-pairs',
-\ 'coc-java',
-\ 'coc-tsserver',
-\ 'coc-eslint',
-\ 'coc-prettier',
-\ 'coc-json',
-\ 'coc-css',
-\ ]
+let g:coc_global_extensions = [
+  \ 'coc-snippets',
+  \ 'coc-pairs',
+  \ 'coc-java',
+  \ 'coc-python',
+  \ 'coc-tsserver',
+  \ 'coc-eslint',
+  \ 'coc-prettier',
+  \ 'coc-json',
+  \ 'coc-css',
+  \ 'coc-html',
+  \ ]
 ]]
--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -3,10 +3,12 @@ try
   syntax enable
   set background=dark
   " let ayucolor="mirage"
-  colorscheme dracula
-  " let g:neosolorized_termtrans=1
-  " runtime ../../colors/NeoSolarized.vim
-  " colorscheme NeoSolarized
+  " colorscheme dracula
+  let g:neosolarized_termtrans=1
+  runtime $HOME/.config/nvim/colors/NeoSolarized.vim
+  colorscheme NeoSolarized
+  highlight Visual cterm=NONE ctermbg=236 ctermfg=NONE guibg=Grey40
+  highlight LineNr cterm=none ctermfg=240 guifg=2b506e guibg=#000000
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
